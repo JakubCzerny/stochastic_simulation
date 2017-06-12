df7c2bc26c4c2135429021b0a38446b6e65ad55f
JakubCzerny <kuba.czerny@poczta.fm>
2017-06-12 16:32:52 +0200

    Removed incorrect method

--- queueing_system/Queueing_system.py
+++ queueing_system/Queueing_system.py
@@ -1,28 +1,28 @@
+from __future__ import division
 import random
-from Customer import Customer_no_queue
+import numpy as np
+from Customer import Customer
 
-class Arrival_process_no_queue(object):
-    def __init__(self, n, m_arrive, m_service, to_be_served):
+class Arrival_process(object):
+    def __init__(self, n, arrival, service, to_be_served):
         self.served = 0
-        self.dropped = 0
+        self.blocked = 0
 
+        self.arrival = arrival
+        self.service = service
         self.n = n
-        self.m_service = m_service
-        self.m_arrive = m_arrive
         self.to_be_served = to_be_served
 
         self.time = 0
         self.init_process()
 
     def init_process(self):
-        # self.customers = [Customer(i, self.m_arrive) for i in range(self.to_be_served)]
-        # self.customers.sort(key=lambda x: x.t_arrival)
-        customer = Customer_no_queue(self.time, self.m_arrive, self.m_service)
+        customer = Customer(self.time, self.arrival.get_value(), self.service.get_value())
         self.customers = [customer]
 
     def simulate(self):
         while self.served < self.to_be_served:
-            self.time += 1
+            self.time += 1 / self.interarrival.mean
 
             for customer in self.customers:
                 if customer.t_next < self.time:
@@ -30,47 +30,45 @@ class Arrival_process_no_queue(object):
                     self.served += 1
 
             if len(self.customers) < self.n:
-                customer = Customer_no_queue(self.time, self.m_arrive, self.m_service)
+                customer = Customer(self.time, self.arrival.get_value(), self.service.get_value())
                 self.customers.append(customer)
             else:
-                self.dropped += 1
+                self.blocked += 1
 
             self.customers.sort(key=lambda x: x.get_next_event_time(self.time))
 
-        print "Served: ",self.served, ' skipped: ',self.dropped
+        print "Served: ",self.served, ' skipped: ',self.blocked
 
-#
-# class Arrival_process_renewal(object):
-#     def __init__(self, n, m_arrive, m_service, to_be_served):
-#         self.served = 0
-#         self.dropped = 0
-#
-#         self.n = n
-#         self.m_service = m_service
-#         self.m_arrive = m_arrive
-#         self.to_be_served = to_be_served
-#
-#         self.time = 0
-#         self.init_process()
-#
-#     def init_process(self):
-#         customer = Customer(self.time, self.m_arrive, self.m_service)
-#         self.customers = [customer]
-#
-#     def simulate(self):
-#         while self.served < self.to_be_served:
-#             print "Served: ",self.served, ' skipped: ',self.dropped
-#             self.time += 1
-#
-#             for customer in self.customers:
-#                 if customer.t_next < self.time:
-#                     self.customers.remove(customer)
-#                     self.served += 1
-#
-#             if len(self.customers) < self.n:
-#                 customer = Customer(self.time, self.m_arrive, self.m_service)
-#                 self.customers.append(customer)
-#             else:
-#                 self.dropped += 1
-#
-#             self.customers.sort(key=lambda x: x.get_next_event_time(self.time))
+
+class Renewal_process(object):
+    def __init__(self, n, interarrival, service, to_be_served):
+        self.served = 0
+        self.blocked = 0
+
+        self.interarrival = interarrival
+        self.service = service
+        self.n = n
+        self.to_be_served = to_be_served
+
+        self.time = 0
+        self.init_process()
+
+    def init_process(self):
+        customer = Customer(self.time, self.interarrival.get_value(), self.service.get_value())
+        self.last_arrival = customer.t_arrival
+        self.customers = [customer]
+
+    def simulate(self):
+        while self.served < self.to_be_served:
+            self.time += 1/self.interarrival.mean
+
+            for customer in self.customers:
+                if customer.t_departure < self.time:
+                    self.customers.remove(customer)
+                    self.served += 1
+
+            customer = Customer(self.time, self.arrival.get_value(), self.service.get_value())
+            if len(self.customers) < self.n:
+                self.customers.append(customer)
+            else:
+                self.blocked += 1
--- queueing_system/ex.py
+++ queueing_system/ex.py
@@ -1,3 +1,69 @@
-from Queueing_system import Arrival_process_no_queue#, Arrival_process_renewal
+from __future__ import division
+from Queueing_system import Arrival_process, Renewal_process
+from numpy.random import poisson, gamma, exponential
+import numpy as np
+import math
+from scipy import stats
 
-systems = [Arrival_process_no_queue(10, 1, 8, 10000).simulate() for i in range(10)]
+class Poisson:
+    def __init__(self, rate):
+        self.rate = rate
+        self.mean = rate
+    def get_value(self):
+        return poisson(self.rate)
+
+class Erlang:
+    def __init__(self, p, rate):
+        self.p = p
+        self.rate = rate
+        self.mean = p / rate
+    def get_value(self):
+        return gamma(self.p, 1/self.rate)
+
+class Exponential:
+    def __init__(self, rate):
+        self.rate = rate
+        self.mean = 1/rate
+    def get_value(self):
+        return exponential(1/self.rate)
+
+tmp = []
+class Hyper_Exponential:
+    def __init__(self, probs, rates):
+        self.probs = probs
+        self.rates = rates
+        self.mean = np.sum(np.array(probs)/np.array(rates))
+    def get_value(self):
+        exps = np.array([exponential(1/rate) for rate in self.rates])
+        tmp.append(np.sum(exps*self.probs))
+        return np.sum(exps*self.probs)
+
+def generate_CI(x):
+    mean = np.mean(x)
+    var = np.var(x)
+    alpha = 0.05
+    ci = stats.t.interval(1-alpha, len(systems)-1, mean, math.sqrt(var))
+    ci = [max(0,ci[0]),ci[1]]
+
+    print "Blocked: ",ci[0],"<", mean,"<",ci[1],'\n'
+
+
+# systems = [Arrival_process(10, Poisson(1), Poisson(8), 10000) for i in range(10)]
+# [system.simulate() for system in systems]
+# generate_CI([system.blocked for system in systems])
+#
+# systems = [Arrival_process(10, Poisson(1), Exponential(1/8), 10000) for i in range(10)]
+# [system.simulate() for system in systems]
+# generate_CI([system.blocked for system in systems])
+#
+# systems = [Renewal_process(10, Erlang(1,1), Exponential(1/8), 10000) for i in range(10)]
+# [system.simulate() for system in systems]
+# generate_CI([system.blocked for system in systems])
+
+# systems = [Renewal_process(10, Hyper_Exponential([0.8, 0.2], [0.8333, 5.0]), Exponential(1/8), 10000) for i in range(10)]
+# [system.simulate() for system in systems]
+# generate_CI([system.blocked for system in systems])
+
+systems = [Renewal_process(10, Hyper_Exponential([0.8, 0.2], [0.8333, 5.0]), Poisson(8), 10000) for i in range(10)]
+[system.simulate() for system in systems]
+generate_CI([system.blocked for system in systems])
